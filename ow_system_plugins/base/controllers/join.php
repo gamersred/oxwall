04d06c8a3dfca9a1a543ad76ab1047d9524e3ea6
gamersred <115495188+gamersred@users.noreply.github.com>
2025-07-15 18:01:33 +0200

    Secure base fields validator 1/3
    Lets secure join callback data by including csrf_token

--- ow_system_plugins/base/controllers/join.php
+++ ow_system_plugins/base/controllers/join.php
@@ -247,6 +247,13 @@ class BASE_CTRL_Join extends OW_ActionController
             throw new Redirect404Exception();
         }
 
+        $csrf_token = $_POST['csrf_token']??null;
+
+if ( $csrf_token == null || !UTIL_Csrf::isTokenValid($csrf_token) )
+        {
+exit(json_encode(['error'=>OW::getLanguage()->text("base", "invalid_csrf_token_error_message")]));
+}
+
         $command = (string) $_POST["command"];
         
         switch ( $command )
